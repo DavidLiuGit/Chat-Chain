f3005b6b1b267cd9fc052bd368cf84bded6d03f7
DL <dvdgege5@gmail.com>
2025-05-05 22:30:48 -0500

    Remove dependency on Runnable; impl chat history converter

--- chat_chain/chain.py
+++ chat_chain/chain.py
@@ -1,8 +1,8 @@
 from typing import Annotated, Optional, Union, Callable
 
 from langchain_core.language_models import BaseLanguageModel
-from langchain_core.runnables import Runnable
 from langchain_core.retrievers import BaseRetriever
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
 from pydantic import BaseModel, Field
 
 from chat_chain.utils.bedrock_api import get_bedrock_client_from_environ
@@ -34,7 +34,7 @@ class ChatChainProps(BaseModel):
 
 
 
-class ChatChain(Runnable):
+class ChatChain:
     
     
     def __init__(
@@ -42,9 +42,38 @@ class ChatChain(Runnable):
         chat_chain_props: ChatChainProps,
     ):
         self.props = chat_chain_props
-    
-    
-    # def invoke(
-    #     self,
         
-    # )
+    
+    def chat(
+        self,
+        user_input: str,
+        chat_history: list[BaseMessage]
+    ) -> str:
+        pass
+    
+    
+    @staticmethod
+    def build_structured_chat_history(unstructured_chat_history: list[tuple[str, str]]) -> list[BaseMessage]:
+        """
+        Convert a list of chat messages to a structured `list[BaseMessage]`, required by the `chat` method.
+        Each tuple in `unstructured_chat_history` is expected to have the format (actor, message_str). e.g.:
+        ```
+        unstructured_chat_history = [
+            ("human", "What did I do on my birthday?"),
+            ("system", "The user stayed at home all day and watched TV."),  # system-msgs are optional
+            ("ai", "Sounds like you had a great time on your birthday!"),
+        ]
+        ```
+        """
+        output: list[BaseMessage] = []
+        for input_msg in unstructured_chat_history:
+            agent = input_msg[0].lower()
+            if agent == "human":
+                output.append(HumanMessage(content=input_msg[1]))
+            elif agent == "ai":
+                output.append(AIMessage(content=input_msg[1]))
+            elif agent == "system":
+                output.append(SystemMessage(content=input_msg[1]))
+            # if the agent is not one of the above, ignore it
+        return output
+    
