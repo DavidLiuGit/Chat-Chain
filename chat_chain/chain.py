53ae2d38eef025a486024aa6259cc3919fdf9cd2
DL <dvdgege5@gmail.com>
2025-05-06 22:14:24 -0500

    Impl chain building logic; add unit tests
    Squashed commit of the following:
    
    commit 7a86565bfddfee835b3f28592599b01536bc47aa
    Author: DL <dvdgege5@gmail.com>
    Date:   Tue May 6 22:13:48 2025 -0500
    
        Format unittest file
    
    commit b54369c919009e37743663a6aaf90b2c3468895a
    Author: DL <dvdgege5@gmail.com>
    Date:   Tue May 6 22:13:30 2025 -0500
    
        Update chat chain composition; add unit tests for chain builder
    
    commit b5b7787451b87bff187c73894e84e79341efc9d9
    Author: DL <dvdgege5@gmail.com>
    Date:   Mon May 5 22:35:47 2025 -0500
    
        Unit tests for build_structured_chat_history
    
    commit f3005b6b1b267cd9fc052bd368cf84bded6d03f7
    Author: DL <dvdgege5@gmail.com>
    Date:   Mon May 5 22:30:48 2025 -0500
    
        Remove dependency on Runnable; impl chat history converter

--- chat_chain/chain.py
+++ chat_chain/chain.py
@@ -1,12 +1,25 @@
+from logging import getLogger
+from textwrap import dedent
 from typing import Annotated, Optional, Union, Callable
 
+from langchain_core.documents import Document
 from langchain_core.language_models import BaseLanguageModel
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import Runnable
 from langchain_core.retrievers import BaseRetriever
 from pydantic import BaseModel, Field
 
 from chat_chain.utils.bedrock_api import get_bedrock_client_from_environ
 from chat_chain.utils.telemetry import _enable_logging
+from chat_chain.utils.chat_prompt_template import (
+    QA_CHAIN_LLM_PROMPT_TEPLATE,
+    QA_CHAIN_LLM_PROMPT_WITH_RETRIEVER_TEMPLATE,
+)
+
+
+logger = getLogger(__name__)
 
 
 class ChatChainProps(BaseModel):
@@ -16,35 +29,123 @@ class ChatChainProps(BaseModel):
             description="The language model to use for chat. Instance of BaseLanguageModel",
         ),
     ]
-    
+
     chat_prompt: Annotated[
         Union[str, Callable[..., str]],
         Field(
-            description="Prompt to use for the LLM. Either a string or a function that returns a string.",
+            description=(
+                "Prompt to use for the LLM. Either a string or a function that returns a string. "
+                "There is no need to include placeholders/templates for user input, chat history, or context. "
+            ),
         ),
     ]
-    
+
     retriever: Annotated[
-        BaseRetriever,
+        Optional[BaseRetriever],
         Field(
-            description="The retriever to use to inject context into a conversation. Instance of BaseRetriever",
+            description=(
+                "(optional) The retriever to use to inject context into a conversation. Instance of BaseRetriever."
+                "If provided, the chat history and latest user input will be provided the the Retriever"
+            ),
         ),
     ]
-    
 
 
+class ChatChain:
 
-class ChatChain(Runnable):
-    
-    
     def __init__(
         self,
         chat_chain_props: ChatChainProps,
     ):
         self.props = chat_chain_props
-    
-    
-    # def invoke(
-    #     self,
-        
-    # )
+        self.qa_chain: Runnable = self._build_question_and_answer_chain()
+
+    def chat(self, user_input: str, chat_history: list[BaseMessage]) -> str:
+        # if a Retriever was provided, use it to attempt to inject context prior to the response
+        context_documents: Optional[list[Document]] = (
+            self.props.retriever.invoke({"user_input": user_input, "chat_history": chat_history})
+            if self.props.retriever
+            else None
+        )
+
+        # feed into chain, extract output
+        return self.qa_chain.invoke(
+            {
+                "user_input": user_input,
+                "chat_history": chat_history,
+                "document_context": context_documents,
+            }
+        )
+
+    @staticmethod
+    def build_structured_chat_history(unstructured_chat_history: list[tuple[str, str]]) -> list[BaseMessage]:
+        """
+        Convert a list of chat messages to a structured `list[BaseMessage]`, required by the `chat` method.
+        Each tuple in `unstructured_chat_history` is expected to have the format (actor, message_str). e.g.:
+        ```
+        unstructured_chat_history = [
+            ("human", "What did I do on my birthday?"),
+            ("system", "The user stayed at home all day and watched TV."),  # system-msgs are optional
+            ("ai", "Sounds like you had a great time on your birthday!"),
+        ]
+        ```
+        """
+        output: list[BaseMessage] = []
+        for input_msg in unstructured_chat_history:
+            agent = input_msg[0].lower()
+            if agent == "human":
+                output.append(HumanMessage(content=input_msg[1]))
+            elif agent == "ai":
+                output.append(AIMessage(content=input_msg[1]))
+            elif agent == "system":
+                output.append(SystemMessage(content=input_msg[1]))
+            # if the agent is not one of the above, ignore it
+        return output
+
+    def _build_question_and_answer_chain(
+        self,
+    ) -> Runnable:
+        """
+        Build the Q&A chain that will be invoked to respond to the user's input.
+        """
+        # build the PromptTemplate
+        self.qa_prompt_template = self._build_qa_llm_prompt_template(self.props.chat_prompt, bool(self.props.retriever))
+        self.chain_input = {
+            "chat_history": lambda x: x["chat_history"],
+            "user_input": lambda x: x["user_input"],
+            # "document_context": lambda x: x["document_context"],
+        }
+        if self.props.retriever and isinstance(self.props.retriever, BaseRetriever):
+            self.chain_input["document_context"] = lambda x: x["document_context"]
+
+        return (
+            self.qa_prompt_template
+            | (lambda x: logger.debug(f"Contextualizer prompt: {x}") or x)  # can enable for debugging, will not fail
+            | self.props.chat_llm
+            | StrOutputParser
+        )
+
+    def _build_qa_llm_prompt_template(
+        self,
+        chat_prompt=Union[str, Callable[..., str]],
+        retriever_available: bool = False,
+    ) -> PromptTemplate:
+        """
+        Create a `PromptTemplate` for the Q&A chain's LLM. If a `Retriever` has been provided, create a placeholder
+        for document context injection.
+        """
+        prompt_str = chat_prompt if isinstance(chat_prompt, str) else chat_prompt()
+        self.prompt_template = PromptTemplate(
+            input_variables=(
+                ["chat_history", "user_input", "document_context"]
+                if retriever_available
+                else ["chat_history", "user_input"]
+            ),
+            partial_variables={
+                "prompt": prompt_str,
+            },
+            template=(
+                QA_CHAIN_LLM_PROMPT_WITH_RETRIEVER_TEMPLATE if retriever_available else QA_CHAIN_LLM_PROMPT_TEPLATE
+            ),
+        )
+        return self.prompt_template
